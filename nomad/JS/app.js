3b7995ebaa6d61951f89c535af12fc7894ad508c
wook888 <guddnr888@gmail.com>
2023-05-28 01:08:11 +0900

    calculator 만들기

---
+++ nomad/JS/app.js
@@ -0,0 +1,15 @@
+const calculator = {
+  plus: function (a, b) {
+    console.log(a + b);
+  },
+  minus: function (a, b) {
+    console.log(a - b);
+  },
+  multipliedBy: function (a, b) {
+    console.log(a * b);
+  },
+  devidedBy: function (a, b) {
+    console.log(a / b);
+  },
+};
+calculator.minus(999, 999);
