4b8779692e5163ed7f0b496aac2a5d16f2d27aa2
wook888 <guddnr888@gmail.com>
2023-06-05 23:56:22 +0900

    프로그래머스 옷가게 할인받기 Math.floor

---
+++ 2306/0605.js
@@ -0,0 +1,12 @@
+function solution(price) {
+  if (price >= 500000) {
+    price *= 0.8;
+  } else if (price >= 300000) {
+    price *= 0.9;
+  } else if (price >= 100000) {
+    price *= 0.95;
+  }
+  return Math.floor(price);
+}
+
+//math.floor로 최종 가격을 정수로 내림하여 반환
