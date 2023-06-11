a4e84a17f604737af237af4b59e3fdaa15d0d289
wook888 <130480277+wook888@users.noreply.github.com>
2023-06-12 03:06:11 +0900

    [level 0] Title: 모음 제거, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120849. 모음 제거/모음 제거.js
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.js
@@ -1,8 +1,3 @@
 function solution(my_string) {
-  var vowels = ['a', 'e', 'i', 'o', 'u'];
-  var filteredString = my_string.split('').filter(function(char) {
-    return vowels.indexOf(char.toLowerCase()) === -1;
-  });
-  var answer = filteredString.join('');
-  return answer;
+    return my_string.replace(/[aeiou]/g, '');
 }
